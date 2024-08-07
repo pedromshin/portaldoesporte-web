33ada061dee2a8838183408e0f7f811af3e562d2
Pedro Shin <pedromaschio.shin@gmail.com>
2024-08-07 15:47:09 -0300

    footer

--- src/pages/components/Footer/index.tsx
+++ src/pages/components/Footer/index.tsx
@@ -1,3 +1,37 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.nav`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
+
+const List = styled.ul`
+  display: flex;
+  padding: 0;
+  margin: 0;
+`;
+
+const Item = styled.div``;
+
 export default () => {
-  return <div></div>;
+  return (
+    <Container>
+      <List>
+        <Item>
+          <Link to="/">Feed</Link>
+        </Item>
+        <Item>
+          <Link to="/following">Acompanhando</Link>
+        </Item>
+        <Item>
+          <Link to="/create-modality">Criar modalidade</Link>
+        </Item>
+        <Item>
+          <Link to="/privacy-policy">Política de privacidade</Link>
+        </Item>
+      </List>
+    </Container>
+  );
 };
